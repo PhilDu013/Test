72ff19f5d2fb03cb068e7a9a5cfdbedf2b4af3d7
Lily Monster <paupertjulie@gmail.com>
2020-04-30 13:31:22 +0200

    login page and styling done

--- routes/index.js
+++ routes/index.js
@@ -42,6 +42,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
 
 // Remplissage de la base de donnée, une fois suffit
 router.get('/save', async function(req, res, next) {
